a85086680c9359b1220c8715227c47dcfd50fe71
Edward Harley <edharley19@gmail.com>
2020-05-27 19:50:33 -0400

    Add: Python the hard way exercises

---
+++ ex5.py
@@ -0,0 +1,19 @@
+my_name = 'Edward Harley'
+my_age = 22  # this is a lie
+my_height = 72  # inches
+my_weight = 200  # lbs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = "None"
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print("Actually that's too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight}, I get {total}.")
+
+

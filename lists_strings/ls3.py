38136d25e59c1e6f89a711891fd630d14b393157
Edward Harley <edharley19@gmail.com>
2020-06-09 22:29:10 -0400

    Completed lists, strings exercises 3 and 4

---
+++ lists_strings/ls3.py
@@ -0,0 +1,10 @@
+def elem_chk(elem, lst):
+    if elem in lst:
+        print("Element exists")
+    else:
+        print("Doesn't exist")
+
+
+a = [1, 3, 9, 4, 5]
+
+elem_chk(1, a)
---
+++ lists_strings/ls4.py
@@ -0,0 +1,6 @@
+def odd_elem(lst):
+    for i in lst:
+        if i % 2 != 0:
+            return i
+
+

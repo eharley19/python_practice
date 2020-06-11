7c16051ec94c9f4a988ed2c67af550cc5775e794
Edward Harley <edharley19@gmail.com>
2020-06-10 22:21:53 -0400

    Corrected solution lists, strings problem 5 - new solution calculates actual running total.

--- lists_strings/ls5.py
+++ lists_strings/ls5.py
@@ -1,7 +1,13 @@
 
 def running_total(lst):
     total = 0
-    for i in lst:
-        total += i
-    return total
+    running_total_list = []
+    for item in lst:
+        total += item
+        running_total_list.append(total)
+    return running_total_list
 
+
+test_list = [1, 2, 3, 4, 5]
+
+assert [1, 3, 6, 10, 15] == running_total(test_list)

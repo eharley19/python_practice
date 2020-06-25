c2bdbf82057ff72ca528a2f15041c5f7c375c871
Edward Harley <edharley19@gmail.com>
2020-06-24 21:06:58 -0400

    Refactored lists, strings problem 12

--- lists_strings/ls12.py
+++ lists_strings/ls12.py
@@ -1,10 +1,19 @@
-def rotate_list(lst):
-    k = input("Rotate by: ")
-    k = int(k)
-    lst = lst[k:] + lst[:k]
-    print(lst)
+def rotate_list(lst, k):
+    m = k % len(lst)
+    return lst[m:] + lst[:m]
+    # new_lst = []
+    # m = k % len(lst)
+    # i = m
+    # while i < len(lst):
+    #     new_lst.append(lst[i])
+    #     i += 1
+    # i = 0
+    # while i < m:
+    #     new_lst.append(lst[i])
+    #     i += 1
+    # return new_lst
 
 
 a = [1, 2, 3, 4, 5, 6]
 
-rotate_list(a)
+assert [3, 4, 5, 6, 1, 2] == rotate_list(a, 38), rotate_list(a, 38)
